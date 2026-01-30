de9071ce9e0788297163f4b7184a18d191e1fdaa
Thomas Erick <thomaserick.dev@gmail.com>
2026-01-29 22:22:17 -0300

    feat: Cria database gerenciador_oficina_notificacao

--- infra-rds-postgres/modules/rds/variables.tf
+++ infra-rds-postgres/modules/rds/variables.tf
@@ -5,7 +5,7 @@ variable "rds_engine_version" { default = "16.6" }
 variable "rds_instance_class" { default = "db.t4g.micro" }
 variable "rds_storage_type" { default = "gp2" }
 variable "db_parameter_group_family" { default = "postgres16" }
-variable "password" { default = "" sensitive = true }
+variable "password" { }
 
 variable "rds_allocated_storage" {
   description = "Armazenamento em GB (Free Tier tipicamente até 20GB)"
