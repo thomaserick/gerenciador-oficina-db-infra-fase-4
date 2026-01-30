34f1115e64126a375b6fceb5f7f16e0dc199a74f
Thomas Erick <thomaserick.dev@gmail.com>
2026-01-30 17:41:39 -0300

    feat: deploy fase 4

--- devops/scripts/create-databases.sh
+++ devops/scripts/create-databases.sh
@@ -3,4 +3,4 @@ set -e
 
 echo "Criando databases no RDS..."
 
-psql "host=$RDS_ENDPOINT port=5432 user=$RDS_USER sslmode=require password=$RDS_PASSWORD" -c "CREATE DATABASE gerenciador-oficina-notificacao;"
+psql "host=$RDS_ENDPOINT port=5432 user=$RDS_USER sslmode=require password=$RDS_PASSWORD" -c "CREATE DATABASE gerenciador_oficina_notificacao;"
