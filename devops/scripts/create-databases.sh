8930ce74b5d606d91170d39d2b1c8cdf27200adb
Thomas Erick <thomaserick.dev@gmail.com>
2026-01-30 16:39:35 -0300

    feat: deploy fase 4

--- devops/scripts/create-databases.sh
+++ devops/scripts/create-databases.sh
@@ -8,4 +8,4 @@ echo $RDS_USER
 echo $RDS_PASSWORD
 
 psql "host=$RDS_ENDPOINT port=5432 dbname=postgres user=$RDS_USER sslmode=require password=$RDS_PASSWORD" \
-  -f scripts/create-databases.sql
+  -f ./scripts/create-databases.sql
