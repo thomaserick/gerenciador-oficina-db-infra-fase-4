31bda6639d794f913042aad607dbb813cc6f71aa
Thomas Erick <thomaserick.dev@gmail.com>
2026-01-30 16:52:06 -0300

    feat: deploy fase 4

--- devops/scripts/create-databases.sh
+++ devops/scripts/create-databases.sh
@@ -7,14 +7,4 @@ echo $RDS_ENDPOINT
 echo $RDS_USER
 echo $RDS_PASSWORD
 
-psql "host=$RDS_ENDPOINT port=5432 user=$RDS_USER sslmode=require password=$RDS_PASSWORD" << EOF
-DO \$\$
-BEGIN
-   IF NOT EXISTS (
-      SELECT FROM pg_database WHERE datname = 'gerenciador_oficina_notificacao'
-   ) THEN
-      CREATE DATABASE gerenciador_oficina_notificacao;
-   END IF;
-END
-\$\$;
-EOF
+psql "host=$RDS_ENDPOINT port=5432 user=$RDS_USER sslmode=require password=$RDS_PASSWORD" -c "CREATE DATABASE gerenciador_oficina_notificacao;"
