462ea26f4eb18098d01697b311e82883ac51c6a1
Thomas Erick <thomaserick.dev@gmail.com>
2026-01-29 22:07:09 -0300

    feat: Cria database gerenciador_oficina_notificacao

---
+++ infra-rds-postgres/scripts/create-databases.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+echo "Criando databases no RDS..."
+
+psql "host=$RDS_ENDPOINT \
+      user=$RDS_USER \
+      password=$RDS_PASSWORD \
+      dbname=postgres sslmode=require" << EOF
+
+DO \$\$
+BEGIN
+
+   IF NOT EXISTS (
+      SELECT FROM pg_database WHERE datname = 'gerenciador_oficina_notificacao'
+   ) THEN
+      CREATE DATABASE gerenciador_oficina_notificacao;
+   END IF;
+END
+\$\$;
+EOF
